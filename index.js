b332ee678f831fd4b2709023a6ff24b72c2b9b10
ParthaMalakar <parthamalakar4321@gmail.com>
2023-12-24 00:34:35 +0600

    create a get api for collect the product information from the database

--- index.js
+++ index.js
@@ -12,33 +12,37 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    const userCollection = client.db("TaskDB").collection("users");
-   
+    try {
+        const userCollection = client.db("ProductManageDB").collection("products");
+        //Product realated API
+        app.get('/products', async (req, res) => {
+            
+            const products = await TaskCollection.find().toArray();
+            res.send(products);
+        });
 
-
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('SERVER is Running');
-  
-  })
-  app.listen(port, () => {
+
+})
+app.listen(port, () => {
     console.log(` web server is running port${port}`)
-  })
+})
