13ee3ad07b5604c7e629b93bf347e083d96c4c1e
ParthaMalakar <parthamalakar4321@gmail.com>
2023-12-24 15:04:46 +0600

    a get api created for find cart information for a specipic user

--- index.js
+++ index.js
@@ -23,19 +23,31 @@ async function run() {
     try {
         const ProductCollection = client.db("ProductManageDB").collection("products");
         const userCollection = client.db("ProductManageDB").collection("users");
-
+        const CartCollection = client.db("ProductManageDB").collection("cart");
         //Product realated API
         app.get('/products', async (req, res) => {
             
             const products = await ProductCollection.find().toArray();
             res.send(products);
         });
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            
+            const result = await CartCollection.insertOne(product);
+            res.send(result);
+          });
         app.get('/Details/:id', async (req, res) => {
             
                 const id = req.params.id;
                 const query = { _id: new ObjectId(id) }
                 const result = await ProductCollection.findOne(query);
                 res.send(result);
+        });
+        app.get('/mycart/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email }
+            const cart = await CartCollection.find(query).toArray();
+            res.send(cart);
         });
         app.post('/users', async (req, res) => {
             const user = req.body;
